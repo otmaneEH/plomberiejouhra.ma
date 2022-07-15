36390cb3cd73a3b4b1f950d59bae9e7c7fe0018d
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-07-15 08:53:31 +0100

    first commit

---
+++ vews/5510a843bc/js/magnific-init.js
@@ -0,0 +1,4 @@
+jQuery(function($){'use strict';$(".blockchain-lightbox, a[data-lightbox^='gal']").magnificPopup({type:'image',mainClass:'mfp-with-zoom',gallery:{enabled:true},zoom:{enabled:true},image:{titleSrc:function(item){var $item=item.el;var $parentCaption=$item.parents('.wp-caption').first();if($item.attr('title')){return $item.attr('title');}
+if($parentCaption){return $parentCaption.find('.wp-caption-text').text();}},},});var selectors='.widget a[href$=".jpeg"],'+
+'.widget a[href$=".jpg"],'+
+'.widget a[href$=".png"]';$(selectors).magnificPopup({type:'image',mainClass:'mfp-with-zoom',gallery:{enabled:false},zoom:{enabled:true},});});
